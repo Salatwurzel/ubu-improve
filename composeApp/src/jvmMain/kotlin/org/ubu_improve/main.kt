7e32d7e401dda1bad21198fbf40903ff62c03203
Salatwurzel <salatwurzel@proton.me>
2025-11-26 12:10:59 +0100

    moved mainFile to root

--- composeApp/src/jvmMain/kotlin/org/ubu_improve/main.kt
+++ composeApp/src/jvmMain/kotlin/org/ubu_improve/main.kt
@@ -1,23 +1,24 @@
-package org.ubu_improve.ui
+package org.ubu_improve
 
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
-import org.jetbrains.compose.resources.painterResource
+import org.ubu_improve.ui.App
 import ubu_improve.composeapp.generated.resources.Res
+import ubu_improve.composeapp.generated.resources.compose_multiplatform
 import ubu_improve.composeapp.generated.resources.wurzelavatar
 
 var currentVersion: String = "0.1-Alpha"
 
 fun main() = application {
-    val icon = painterResource("wurzelavatar.png")
+    val appIcon = org.jetbrains.compose.resources.painterResource(Res.drawable.wurzelavatar)
     Window(
         onCloseRequest = ::exitApplication,
         title = "ubu-improve",
         state = WindowState(),
-        icon = icon
+        icon = appIcon
     ) {
-        App()
+        org.ubu_improve.ui.App()
     }
 }
