f88a195fdfcf3febaae1c5339fb7c8be66f6adb5
Salatwurzel <salatwurzel@proton.me>
2025-11-26 12:38:46 +0100

    changed startmessage

--- composeApp/src/jvmMain/kotlin/org/ubu_improve/logic/installDotnet.kt
+++ composeApp/src/jvmMain/kotlin/org/ubu_improve/logic/installDotnet.kt
@@ -2,18 +2,17 @@ package org.ubu_improve.logic
 
 fun installDotnet() {
     val startMessages = listOf(
-        "Press any key to download and run the dotnet installer"
+        "--------------------------------------------------------",
+        "INFO: THIS WILL DOWNLOAD AND RUN THE DOTNET INSTALLER",
+        "--------------------------------------------------------",
+        "Press any key to continue"
     )
 
     val commands = listOf(
         "wget https://dot.net/v1/dotnet-install.sh -O /tmp/dotnet-install.sh",
         "chmod +x /tmp/dotnet-install.sh",
-        "/tmp/dotnet-install.sh"
+        "sh /tmp/dotnet-install.sh"
     )
 
-    runShellCommands(
-        commands = commands,
-        pressEnterBeforeStart = true,
-        messagesBeforeStart = startMessages
-    )
+    runShellCommands(commands = commands, pressEnterBeforeStart = true, messagesBeforeStart = startMessages)
 }
