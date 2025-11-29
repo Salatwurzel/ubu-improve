ba6244c710167ad3ae465bccdb31d110c7130852
Salatwurzel <salatwurzel@proton.me>
2025-11-29 17:52:02 +0100

    .

--- composeApp/src/jvmMain/kotlin/org/ubu_improve/main.kt
+++ composeApp/src/jvmMain/kotlin/org/ubu_improve/main.kt
@@ -18,10 +18,7 @@ var currentVersion: String = "0.1-Alpha"
 
 fun main() = application {
     Window(
-        onCloseRequest = {
-            exitProcess(0)
-                         }
-        ,
+        onCloseRequest = {exitProcess(0)},
         title = "ubu-improve",
         state = WindowState(size = DpSize(760.dp, 630.dp)),
         icon = org.jetbrains.compose.resources.painterResource(Res.drawable.wurzelavatar),
